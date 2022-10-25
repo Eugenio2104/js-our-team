5f6d3d602a725b983fa74c0e9a298cb7958c0343
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2022-10-25 15:24:55 +0200

    inserimento img

---
+++ milestone1/js/script.js
@@ -0,0 +1,39 @@
+//**MILESTONE 1:**
+//Stampare su console le informazioni di nome, ruolo e la stringa della foto
+const row = document.querySelector(".row")
+
+
+const team = [
+  {
+    name:`Wayne Barnet`,
+    role: `Founder & CEO`,
+    immagine : `<img src="img/wayne-barnett-founder-ceo.jpg" alt="">`
+  },
+  {
+    name:`Angela Caroll`,
+    role: `Chief Editor`,
+    immagine : `<img src="img/angela-caroll-chief-editor.jpg" alt="">`
+  },
+  {
+    name:`Walter Gordon`,
+    role: `Office Manager`,
+    immagine : `<img src="img/walter-gordon-office-manager.jpg" alt="">`
+  },
+  {
+    name:`Angela Lopez`,
+    role: `Social Media Manager`,
+    immagine : `<img src="img/angela-lopez-social-media-manager.jpg" alt="">`,
+  },
+  {
+    name:`Scott Estrada`,
+    role: `Developer`,
+    immagine : `<img src="img/scott-estrada-developer.jpg" alt="">`
+  },
+  {
+    name:`Barbara Ramos`,
+    role: `Graphic Designer`,
+    <img src="img/barbara-ramos-graphic-designer.jpg" alt="">
+  },
+];
+
+console.log(team);
