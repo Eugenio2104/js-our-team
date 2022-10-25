b814491b3c2364807be4aea3a80239eccda102ca
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2022-10-25 14:57:20 +0200

    fine milestone0

--- milestone0/js/script.js
+++ milestone0/js/script.js
@@ -1,1 +1,31 @@
-console.log(`ciao`);
+// **MILESTONE 0:**
+// Creare l’array di oggetti con le informazioni fornite.
+
+const team = [
+  {
+    name:`Wayne Barnet`,
+    role: `Founder & CEO`
+  },
+  {
+    name:`Angela Caroll`,
+    role: `Chief Editor`
+  },
+  {
+    name:`Walter Gordon`,
+    role: `Office Manager`
+  },
+  {
+    name:`Angela Lopez`,
+    role: `Social Media Manager`
+  },
+  {
+    name:`Scott Estrada`,
+    role: `Developer`
+  },
+  {
+    name:`Barbara Ramos`,
+    role: `Graphic Designer`
+  },
+];
+
+console.log(team);
